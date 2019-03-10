73a0e43997fadd47d3ef90950727bac0bb329b14
hwang <hgs0426@gmail.com>
2019-03-10 23:19:31 +0900

    add query condition

--- api/stores/convenience-stores/controllers.js
+++ api/stores/convenience-stores/controllers.js
@@ -1,8 +1,37 @@
 const Database = require('../../../libs/Database'); 
-const getConvenienceStoresFromDb = () => {
+
+const setLimitConditions = (query) => {
+  let conditions = '';
+  if (query.from && query.to) {
+    conditions += `LIMIT ${query.from}, ${query.to} `;
+  }
+  if (!query.from && query.to) {
+    conditions += `LIMIT ${query.to} `;
+  }
+  return conditions;
+}
+
+const setWhereConditions = (query) => {
+  let conditions;
+  conditions = '';
+  if (query.name) {
+    conditions += `WHERE name like "%${query.name}%" `;
+  }
+
+  if (query.region) {
+    conditions += `AND road_address like "%${query.region}%" `;
+  }
+  return conditions;
+};
+
+const getConvenienceStoresFromDb = (where, limit) => {
   return new Promise( async (resolve, reject) => {
     const client = new Database('stores');
-    const sql = `SELECT * FROM convenience_stores`;
+    const sql = `SELECT name, road_address, latitude, longitude
+                 FROM convenience_stores
+                 ${where}
+                 ${limit}`;
+    // console.log(sql);
     try {
       const convenientStores = await client.query(sql);
       resolve(convenientStores);
@@ -13,10 +42,12 @@ const getConvenienceStoresFromDb = () => {
 };
 
 const serveConvenienceStores = async (req, res) => {
-  convenientStores = await getConvenienceStoresFromDb();
+  const where = setWhereConditions(req.query);
+  const limit = setLimitConditions(req.query)
+  convenientStores = await getConvenienceStoresFromDb(where, limit);
   res.json(convenientStores)
 };
 
 module.exports = {
-    serveConvenienceStores
-  };
+  serveConvenienceStores
+};
